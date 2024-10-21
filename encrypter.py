09e0b1609e95742c72cd0f1f8d125e0749eb5d3f
TheQuantumSushi <signup.trashemail@proton.me>
2024-10-21 13:37:45 +0200

    Added Encrypter class to manage sudo password hashing and storage

---
+++ encrypter.py
@@ -0,0 +1,56 @@
+import keyring
+from argon2 import PasswordHasher
+
+class Encrypter:
+  """
+  An encrypter class that handles sudo passwords by hashing them through Argon2
+  (with parameters adaptated from machine specs) and safely storing the through keyring
+  """
+  def __init__(self):
+    self.ph = PasswordHasher()
+
+  def adjust_argon2_parameters():
+    """
+    Retrieve system specifications and user parameters (maximum values allowed) to adjust Argon2 parameters dynamically
+    """
+
+    # Get system specifications :
+    cpu_cores = psutil.cpu_count(logical = False)  # Number of cpu physical cores
+    total_memory = psutil.virtual_memory().total // (1024 * 1024)  # Memory ammount in MB
+    cpu_freq = psutil.cpu_freq().current  # Current CPU frequency in MHz
+
+    # Get user parameters from config.json :
+    with open("config.json") as config_file:
+      config = json.load(config_file)
+    memory_cost_cap = config["hash_parameters"]["memory_cost_cap"]
+    time_cost_cap = config["hash_parameters"]["time_cost_cap"]
+    parallelism_cost_cap = config["hash_parameters"]["parallelism_cost_cap"]
+
+    # Adjust Argon2 parameters :
+    # Get the best system parameters, but do not exceed user cap (which is defined if different than 0)
+
+    # Memory cost : 100MB by default, 4+GB RAM -> 250MB, 8+GB RAM-> 500MB
+    system_based_memory_cost = 512000 if total_memory > 8192 else (256000 if total_memory > 4096 else 102400)
+    memory_cost = min(system_based_memory_cost, memory_cost_cap) if memory_cost_cap else system_based_memory_cost
+    # Time cost : 1 iteration by default, frequency>1500GHz -> 2, frequency>2500GHz -> 3
+    system_based_time_cost = 3 if cpu_freq > 2500 else (2 if cpu_freq > 1500 else 1)
+    time_cost = min(system_based_time_cost, time_cost_cap) if time_cost_cap else system_based_time_cost
+    # Parralelism : at most 8 threads, but not more than the system has
+    parallelism = min(min(cpu_cores, 8), parallelism_cost_cap) if parallelism_cost_cap else min(cpu_cores, 8)
+
+    # Update hasher :
+    self.ph = PasswordHasher(time_cost = time_cost, memory_cost = memory_cost, parallelism = parallelism)
+
+  def encrypt_password(password):
+    """
+    Encrypt the password using Argon2 hashing and store it through keyring
+    """
+    hashed_password = self.ph.hash(password)
+    keyring.set_password("system", "sudo_hashed", "hashed_password")
+
+  def check_password(checked_password):
+    """
+    Check if the provided password is correct
+    """
+    hashed_password = keyring.get_password("system", "sudo_hashed")
+    return self.ph.verify(hashed_password, checked_password)
